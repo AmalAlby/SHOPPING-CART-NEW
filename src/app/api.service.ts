bdc03d64fc116c391bdf57bbe981b630da7e3a8f
AmalAlby <amalalbyn@gmail.com>
2022-12-08 08:45:59 +0530

    bug fixed

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -23,4 +23,8 @@ export class ApiService {
   adduser=(userdata:any)=>{
     return this.http.post("http://localhost:8080/addu",userdata)
   }
+
+  viewuser=()=>{
+    return this.http.get("http://localhost:8080/viewu")
+  }
 }
